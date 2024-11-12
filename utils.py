f9d41834e69da753a665cfb803228c6a767d20a7
MiguelGaspar01 <miguelgaspar01@outlook.com>
2024-11-12 09:35:31 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -18,7 +18,8 @@ def missing_values_plot(data: pd.DataFrame, target: str):
     Raises:
         ValueError: If `target` is not in `data.columns`.
     """
-
+    print(usable.shape)
+    
     if target not in data.columns:
         raise ValueError(f"Target column '{target}' not found in data.")
 
